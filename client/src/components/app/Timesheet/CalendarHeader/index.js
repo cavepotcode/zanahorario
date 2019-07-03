a929b7a0dc0c19aa8df62ce774bc543cfb24901c
Matias Delgado <matiasdel@gmail.com>
2019-07-03 20:14:52 -0300

    Timesheet: refactor grid css

--- client/src/components/app/Timesheet/CalendarHeader/index.js
+++ client/src/components/app/Timesheet/CalendarHeader/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from './styles.module.scss';
+import grid from '../grid.module.scss';
 import { getWeekdayName } from '../../../../utils/date';
 import classes from '../../../../utils/classes';
 
 export default function CalendarHeader({ startDate }) {
   const week = new Array(7).fill(0);
   return (
-    <div className={styles.container}>
-      <div className={styles.title}>Project</div>
+    <div className={classes(grid.row, styles.container)}>
+      <div className={classes(grid.cell, grid.first, styles.title)}>Project</div>
       {week.map((_, index) => {
         const date = new Date(startDate);
         date.setDate(date.getDate() + index);
@@ -22,7 +23,7 @@ function CalendarCard({ date, index }) {
   const isWeekend = [0, 6].includes(date.getDay());
   const name = getWeekdayName(date);
   return (
-    <div className={classes(styles.date, isWeekend && styles.secondary)}>
+    <div className={classes(grid.cell, styles.date, isWeekend && styles.secondary)}>
       <div>{weekdate}</div>
       <div>{name}</div>
     </div>
--- client/src/components/app/Timesheet/ProjectLine/index.js
+++ client/src/components/app/Timesheet/ProjectLine/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from './styles.module.scss';
+import grid from '../grid.module.scss';
+import classes from '../../../../utils/classes';
 
 export default function ProjectLine({ project }) {
   const week = new Array(7).fill(0);
   return (
-    <div className={styles.container}>
-      <div className={styles.title}>{project.name}</div>
+    <div className={classes(styles.container, grid.row)}>
+      <div className={classes(styles.title, grid.cell, grid.first)}>{project.name}</div>
       {week.map(() => (
-        <input />
+        <input className={grid.cell} />
       ))}
     </div>
   );
