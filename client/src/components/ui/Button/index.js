1798d654fa150bcc6e8da9e11bc90c88808b4afe
Matias Delgado <matiasdel@gmail.com>
2019-07-23 14:34:05 -0300

    Remove typing

--- client/src/components/ui/Button/index.js
+++ client/src/components/ui/Button/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import styles from './styles.module.scss';
 
-type Props = {
-  children: React.Node,
-  disabled?: boolean,
-  onClick?: Function,
-  type?: string
-};
-export default function Button({ children, onClick = () => null, type = 'submit', ...props }: Props) {
+export default function Button({ children, onClick = () => null, type = 'submit', ...props }) {
   return (
     <button onClick={onClick} type={type} className={styles.button} {...props}>
       {children}
